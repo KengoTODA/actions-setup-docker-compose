0569cf4fbb685ddcd8539d4355a35d471b342d3f
Sergeev Alexander <shveitsar215@gmail.com>
2022-04-17 17:42:42 +0300

    Version tag for releases >= 2.0.0 fixed

--- src/install.ts
+++ src/install.ts
@@ -17,19 +17,22 @@ export async function runCommand(command: string): Promise<string> {
 }
 
 async function installOnLinux(version: string): Promise<string> {
-  const system = await runCommand('uname -s')
-  const hardware = runCommand('uname -m')
-  const casedSystem = parseInt(version.split('.')[0], 10) >= 2 ? `${system[0].toUpperCase()}${system.slice(1)}` : system
-  const url = `https://github.com/docker/compose/releases/download/${version}/docker-compose-${casedSystem}-${await hardware}`
-  const installerPath = await downloadTool(url)
-  await exec(`chmod +x ${installerPath}`)
-  const cachedPath = await cacheFile(
-    installerPath,
-    'docker-compose',
-    'docker-compose',
-    version
-  )
-  return cachedPath
+    let system = await runCommand('uname -s')
+    const hardware = runCommand('uname -m')
+    if (parseInt(version.split('.')[0], 10) >= 2) {
+        system = `${system[0].toUpperCase()}${system.slice(1)}`
+        version = `v${version}`
+    }
+    const url = `https://github.com/docker/compose/releases/download/${version}/docker-compose-${system}-${await hardware}`
+    const installerPath = await downloadTool(url)
+    await exec(`chmod +x ${installerPath}`)
+    const cachedPath = await cacheFile(
+        installerPath,
+        'docker-compose',
+        'docker-compose',
+        version
+    )
+    return cachedPath
 }
 
 export async function install(version: string): Promise<string> {
