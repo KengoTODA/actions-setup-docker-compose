85d48d6d3b8b370e5603bde8ac31faafb16c154e
Kengo TODA <skypencil@gmail.com>
2022-12-25 11:35:38 +0800

    implement the necessary feature
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import * as core from '@actions/core'
 import {install} from './install'
+import {Octokit} from '@octokit/action'
+
+async function findLatestVersion() {
+  const octokit = new Octokit()
+  const response = await octokit.repos.getLatestRelease({
+    owner: 'docker',
+    repo: 'compose'
+  })
+  return response.data.tag_name
+}
 
 async function run(): Promise<void> {
   try {
-    const version: string = core.getInput('version', {
+    let version: string = core.getInput('version', {
       required: true
     })
+    if (version === 'latest') {
+      version = await findLatestVersion()
+    }
     const commandPath = await install(version)
     core.addPath(commandPath)
   } catch (error) {
