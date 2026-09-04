13009a0e7935caf0b2262d510b13421c950da0d2
Kengo TODA <toda_k@henry.jp>
2026-09-04 21:10:30 +0800

    chore(deps): update GitHub Actions dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,14 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': [
+      'ts-jest',
+      {useESM: true, tsconfig: {module: 'ESNext', moduleResolution: 'Bundler'}}
+    ]
   },
   moduleNameMapper: {
     '^@octokit/core$': '<rootDir>/__tests__/__mocks__/@octokit/core.js',
